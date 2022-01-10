5a81c474fced0fc2bfcb21ec17956e91cb646df9
okiki123 <okikimalomo17@gmail.com>
2022-01-10 23:49:20 +0100

    Updated Commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,43 @@
-import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
-import { Layout, Typography, Space } from "antd";
-import {
-  Navbar,
-  Exchanges,
-  Homepage,
-  CryptoDetails,
-  Cryptocurrencies,
-  News,
-} from "./components";
-import "./App.css";
-const App = () => {
-  return (
-    <div className="app">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="main">
-        <Layout>
-          <div className="routes">
-            <Switch>
-              <Route exact path="/">
-                <Homepage />
-              </Route>
-              <Route exact path="/exchanges">
-                <Exchanges />
-              </Route>
-              <Route exact path="/cryptocurrencies">
-                <Cryptocurrencies />
-              </Route>
-              <Route exact path="/crypto/:coinId">
-                <CryptoDetails />
-              </Route>
-              <Route exact path="/news">
-                <News />
-              </Route>
-            </Switch>
-          </div>
-        </Layout>
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
+    </div>
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route exact path="/exchanges">
+              <Exchanges />
+            </Route>
+            <Route exact path="/cryptocurrencies">
+              <Cryptocurrencies />
+            </Route>
+            <Route exact path="/crypto/:coinId">
+              <CryptoDetails />
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>
+          </Switch>
+        </div>
+      </Layout>
       <div className="footer">
-        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center'}}>
-          Cryptoverse <br /> All rights reserved
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="/">
+            Cryptoverse Inc.
+          </Link> <br />
+          All Rights Reserved.
         </Typography.Title>
         <Space>
           <Link to="/">Home</Link>
@@ -48,9 +45,8 @@ const App = () => {
           <Link to="/news">News</Link>
         </Space>
       </div>
-      </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default App;
